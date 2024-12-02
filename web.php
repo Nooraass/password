814e67ee0e66bf7927df072486a498d851e3d14b
Nooraass <nooralnahdi113@gmail.com>
2024-12-02 12:30:59 +0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('N', [AuthController::class, 'create'])->name('N.create');
+Route::post('N', [AuthController::class, 'store'])->name('N.store');
